7906aea80070bfaacf235117715de1a0e6e72bd4
claudio <vitorioc86@gmail.com>
2021-06-21 22:26:48 -0300

    04 - Manipulando Datadase

--- app/src/main/java/com/claudiovitorio/firebaseapp/MainActivity.java
+++ app/src/main/java/com/claudiovitorio/firebaseapp/MainActivity.java
@@ -1,22 +1,35 @@
 package com.claudiovitorio.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.claudiovitorio.firebaseapp.model.Upload;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 private FirebaseAuth auth = FirebaseAuth.getInstance();
-private Button btnLogout;
+private Button btnLogout,btnStorage;
+private DatabaseReference database = FirebaseDatabase.getInstance().getReference("Uploads");
+private ArrayList<Upload> listaUploads = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogout = findViewById(R.id.main_btn_logout);
+        btnStorage = findViewById(R.id.main_btn_storage);
 
         btnLogout.setOnClickListener(v -> {
             //deslogar usuario
@@ -29,5 +42,41 @@ private Button btnLogout;
 
         TextView  textNome = findViewById(R.id.main_text_user);
         textNome.setText(auth.getCurrentUser().getDisplayName());
+
+        btnStorage.setOnClickListener( v-> {
+
+            Intent i = new Intent(getApplicationContext(),StorageActivity.class);
+            startActivity(i);
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        // onStart -> faz parte do cliclo de vida de uma Activity
+        // -> e executado depois do OnCreate
+        // -> é executado quando o app incia,
+        // -> e quando volta do background
+        super.onStart();
+        getData();
+    }
+    public void getData(){
+        //listener p/ nó uploads
+        // - caso ocorra alguma alteracao -> retorna tODOS os dados!!
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for( DataSnapshot no_filho : snapshot.getChildren()){
+                    Upload upload = no_filho.getValue(Upload.class);
+                    listaUploads.add(upload);
+                    Log.i("DATABASE", "id:"+ upload.getId()+ ", nome:" + upload.getNomeImagem());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/claudiovitorio/firebaseapp/StorageActivity.java
+++ app/src/main/java/com/claudiovitorio/firebaseapp/StorageActivity.java
@@ -1,25 +1,159 @@
 package com.claudiovitorio.firebaseapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.claudiovitorio.firebaseapp.model.Upload;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
 
 public class StorageActivity extends AppCompatActivity {
-private FirebaseStorage storage=FirebaseStorage.getInstance();
-private Button btnUpload;
+    //referencia p/ FirebaseStorage
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private Button btnUpload,btnGaleria;
+    private ImageView imageView;
+    private Uri imageUri=null;
+    private EditText editNome;
+    // referencia p/ um nó RealtimeDB
+    private DatabaseReference database = FirebaseDatabase.getInstance().getReference("Uploads");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
-
         btnUpload = findViewById(R.id.storage_btn_upload);
-        btnUpload.setOnClickListener(v -> {
-          Uri uri = Uri.parse("https://github.com/clemiltonfucapi/DispMoveisII-2021-02/blob/main/Firebase%20Storage/celular.jpg");
-          storage.getReference().putFile(uri);
+        imageView = findViewById(R.id.storage_image_cel);
+        btnGaleria = findViewById(R.id.storage_btn_galeria);
+        editNome = findViewById(R.id.storage_edit_nome);
+
+        btnUpload.setOnClickListener(v ->{
+            if(editNome.getText().toString().isEmpty()){
+                Toast.makeText(this,"Digite um nome para Imagem",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(imageUri!=null){
+                uploadImagemUri();
+            }else {
+                uploadImagemByte();
+            }
         });
+
+        btnGaleria.setOnClickListener( v -> {
+            Intent intent = new Intent();
+            //intent implicita -> pegar um arquivo do celular
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent,112);
+        });
+
+    }
+
+    private void uploadImagemUri() {
+        String tipo = getFileExtension(imageUri);
+        //referencia do arquivo no firebase
+        Date d = new Date();
+        String nome = editNome.getText().toString();
+        //Criando referencia da imagem no Storage
+        StorageReference imagemRef = storage.getReference()
+                .child("imagens/"+nome+"-"+d.getTime()+"."+tipo);
+
+        imagemRef.putFile(imageUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    Toast.makeText(this,"Upload feito com sucesso",
+                            Toast.LENGTH_SHORT).show();
+
+                    //inserir dados da imagem no RealtimeDatabase
+
+                    //pegar a URL da imagem
+                    taskSnapshot.getStorage().getDownloadUrl()
+                            .addOnSuccessListener(uri -> {
+                                //inserir no database
+                                //criando referencia(datbase) do upload
+                                DatabaseReference refUpload = database.push();
+                                String id = refUpload.getKey();
+
+                                Upload upload = new Upload(id,nome,uri.toString());
+                                //Slavndo upload no db
+                                refUpload.setValue(upload);
+
+
+                            });
+
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+
+    }
+    //retornar o tipo(.png, .jpg) da imagem
+    private String getFileExtension(Uri imageUri) {
+        ContentResolver cr = getContentResolver();
+        return MimeTypeMap.getSingleton()
+                .getExtensionFromMimeType(cr.getType(imageUri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("RESULT", "requestCode: "+ requestCode +
+                ",resultCode: "+ resultCode);
+        if(requestCode==112 && resultCode== Activity.RESULT_OK){
+            //caso o usuario selecionou uma imagem da galeria
+
+            //endereco da imagem selecionada
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+    public byte[] convertImage2Byte(ImageView imageView){
+        //Converter ImageView -> byte[]
+        Bitmap bitmap = ( (BitmapDrawable) imageView.getDrawable() ).getBitmap();
+        //objeto baos -> armazenar a imagem convertida
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        return baos.toByteArray();
+    }
+    //fazer um upload de uma imagem convertida p/ bytes
+    public void uploadImagemByte(){
+        byte[] data = convertImage2Byte(imageView);
+
+        //Criar uma referencia p/ imagem no Storage
+        StorageReference imagemRef = storage.getReference()
+                .child("imagens/01.jpeg");
+        //Realiza o upload da imagem
+        imagemRef.putBytes(data)
+                .addOnSuccessListener(taskSnapshot -> {
+
+                    Toast.makeText(this, "Upload feito com sucesso!",
+                            Toast.LENGTH_SHORT).show();
+                    Log.i("UPLOAD","Sucesso");
+
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+
+
     }
 }
